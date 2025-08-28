9aaee43433172162edd3088befd1cf9617e34d6a
uhaha-fanfare <joonseo@gmail.com>
2025-08-28 10:35:10 +0900

    added output parsing

--- main.py
+++ main.py
@@ -5,20 +5,32 @@ load_dotenv()
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
-# from langchain_openai import ChatOpenAI
-# from langchain_ollama import ChatOllama
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
+# from langchain_openai import ChatOpenAI
+# from langchain_ollama import ChatOllama
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 tools = [TavilySearch()]
 # llm = ChatOpenAI(model="gpt-4")
 # llm = ChatOllama(model="gemma3:270m")
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 react_promt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
+
 agent = create_react_agent(
-    tools=tools,
     llm=llm,
-    prompt=react_promt
+    tools=tools,
+    prompt=react_prompt_with_format_instructions,
 )
 agent_excuter=AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
 chain=agent_excuter
